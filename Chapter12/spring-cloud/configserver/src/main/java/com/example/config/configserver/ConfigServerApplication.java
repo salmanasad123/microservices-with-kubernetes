9db157a5e54d8abc5cfff8656049b1b4a0be7f05
BH20584 <salman.asad@macys.com>
2025-11-18 21:01:27 +0500

    Added Spring Config server fixes

--- Chapter12/spring-cloud/configserver/src/main/java/com/example/config/configserver/ConfigServerApplication.java
+++ Chapter12/spring-cloud/configserver/src/main/java/com/example/config/configserver/ConfigServerApplication.java
@@ -1,8 +1,11 @@
 package com.example.config.configserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  *  The config server supports the storing of configuration files in a number of different backends, for example:
@@ -35,8 +38,13 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class ConfigServerApplication {
 
+	private static final Logger LOG = LoggerFactory.getLogger(ConfigServerApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(ConfigServerApplication.class, args);
+
+		String repoLocation = ctx.getEnvironment().getProperty("spring.cloud.config.server.native.searchLocations");
+		LOG.info("Serving configurations from folder: " + repoLocation);
 	}
 
 }
